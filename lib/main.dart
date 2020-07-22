729fc1664645e2f55ad0197345cda854ba40bf50
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-07-22 14:07:16 -0300

    calculations working

--- lib/main.dart
+++ lib/main.dart
@@ -12,60 +12,99 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController pesoControle = TextEditingController();
+  TextEditingController alturaControle = TextEditingController();
+
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    pesoControle.text = "";
+    alturaControle.text = "";
+    _infoText = "Informe seus dados!";
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(pesoControle.text);
+      double height = double.parse(alturaControle.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Um pouco acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora IMC dart"),
-        centerTitle: true,
-        backgroundColor: Colors.yellow,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      backgroundColor: Colors.green,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person, size: 120, color: Colors.white),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Peso(kg)",
-                  labelStyle: TextStyle(color: Colors.white)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 25.0),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Altura(cm)",
-                  labelStyle: TextStyle(color: Colors.white)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 25.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10, bottom: 10),
-              child: Container(
-                height: 50,
-                child: RaisedButton(
-                  onPressed: (){},
-                  child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0),),
-                  color: Colors.yellow,
-                ),
-              ),
-            ),
-            Text("info",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 25.0),)
+        appBar: AppBar(
+          title: Text("Calculadora IMC dart"),
+          centerTitle: true,
+          backgroundColor: Colors.yellow,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFields,
+            )
           ],
         ),
-      )
-    );
+        backgroundColor: Colors.green,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person, size: 120, color: Colors.white),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso(kg)",
+                    labelStyle: TextStyle(color: Colors.white)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+                controller: pesoControle,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura(cm)",
+                    labelStyle: TextStyle(color: Colors.white)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+                controller: alturaControle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Container(
+                  height: 50,
+                  child: RaisedButton(
+                    onPressed: _calculate,
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              )
+            ],
+          ),
+        ));
   }
 }
