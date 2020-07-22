2c8c55dd71660c251a445e8e7c619e568b456327
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-07-22 15:19:55 -0300

    validations working correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// Created by Guilherme Coutinho @ 2020
 
 void main() {
   runApp(MaterialApp(
@@ -15,12 +16,16 @@ class _HomeState extends State<Home> {
   TextEditingController pesoControle = TextEditingController();
   TextEditingController alturaControle = TextEditingController();
 
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
   String _infoText = "Informe seus dados!";
 
   void _resetFields() {
     pesoControle.text = "";
     alturaControle.text = "";
-    _infoText = "Informe seus dados!";
+    setState(() {
+      _infoText = "Informe seus dados!";
+    });
   }
 
   void _calculate() {
@@ -30,11 +35,14 @@ class _HomeState extends State<Home> {
       double imc = weight / (height * height);
       print(imc);
       if (imc < 18.6) {
-        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está abaixo do peso ideal (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso ideal (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está com o peso ideal (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Um pouco acima do peso (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está um pouco acima do peso (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 29.9 && imc < 34.9) {
         _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 34.9 && imc < 39.9) {
@@ -49,9 +57,9 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Calculadora IMC dart"),
+          title: Text("Calculadora IMC"),
           centerTitle: true,
-          backgroundColor: Colors.yellow,
+          backgroundColor: Colors.black12,
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.refresh),
@@ -59,51 +67,67 @@ class _HomeState extends State<Home> {
             )
           ],
         ),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.white12,
         body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(Icons.person, size: 120, color: Colors.white),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Peso(kg)",
-                    labelStyle: TextStyle(color: Colors.white)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 25.0),
-                controller: pesoControle,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Altura(cm)",
-                    labelStyle: TextStyle(color: Colors.white)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 25.0),
-                controller: alturaControle,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 10),
-                child: Container(
-                  height: 50,
-                  child: RaisedButton(
-                    onPressed: _calculate,
-                    child: Text(
-                      "Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.android, size: 120, color: Colors.green),
+                TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso(kg)",
+                        labelStyle: TextStyle(color: Colors.white)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                    controller: pesoControle,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira seu peso!";
+                      }
+                    }),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura(cm)",
+                      labelStyle: TextStyle(color: Colors.white)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  controller: alturaControle,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira sua Altura!";
+                    }
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Container(
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: (){
+                        if(_formkey.currentState.validate()){
+                          _calculate();
+                        }
+                      },
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                      color: Colors.black12,
                     ),
-                    color: Colors.yellow,
                   ),
                 ),
-              ),
-              Text(
-                _infoText,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 25.0),
-              )
-            ],
+                Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                )
+              ],
+            ),
           ),
         ));
   }
