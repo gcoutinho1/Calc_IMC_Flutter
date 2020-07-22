963e242b2c324a0f548d364c1a4f14c2e56870a3
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-07-22 13:24:48 -0300

    Added ScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -27,28 +27,45 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.green,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person, size: 120, color: Colors.white),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso(kg)",
-                labelStyle: TextStyle(color: Colors.white)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura(cm)",
-                labelStyle: TextStyle(color: Colors.white)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white, fontSize: 25.0),
-          ),
-        ],
-      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person, size: 120, color: Colors.white),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso(kg)",
+                  labelStyle: TextStyle(color: Colors.white)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura(cm)",
+                  labelStyle: TextStyle(color: Colors.white)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 25.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Container(
+                height: 50,
+                child: RaisedButton(
+                  onPressed: (){},
+                  child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            Text("info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 25.0),)
+          ],
+        ),
+      )
     );
   }
 }
